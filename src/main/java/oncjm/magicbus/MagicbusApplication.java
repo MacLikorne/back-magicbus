9f04a51eef8f471a9e658297519e150b819a1a04
Enzo <e.radnai@hotmail.fr>
2020-10-22 22:09:10 +0200

    init commit

---
+++ src/main/java/oncjm/magicbus/MagicbusApplication.java
@@ -0,0 +1,13 @@
+package oncjm.magicbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oncjm/magicbus/MagicbusApplicationTests.java
@@ -0,0 +1,13 @@
+package oncjm.magicbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
